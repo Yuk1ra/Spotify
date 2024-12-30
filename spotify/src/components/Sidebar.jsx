1683e91d8812f6484e1e0c6f436b6d27ea904baa
Yuk1ra <134232026+Yuk1ra@users.noreply.github.com>
2024-12-30 16:12:54 +0200

    Added Sidebar

---
+++ spotify/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import '../style/Sidebar.css'
+
+export default function Sidebar(){
+    return(
+        <div class ="sidebar">
+            <h4>Playlists</h4>
+            <button class = "listbutton">Playlist1</button>
+            <button class = "listbutton">Playlist2</button>
+            <button class = "listbutton">Playlist3</button>
+        </div>
+    )
+}
