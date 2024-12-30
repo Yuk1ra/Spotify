9e043d338616d427fd16d3a7ca124c1d32bf7d85
hqshe <114933368+hqshe@users.noreply.github.com>
2024-12-30 20:58:38 +0200

    Squashed commit of the following:
    commit 1683e91d8812f6484e1e0c6f436b6d27ea904baa
    Author: Yuk1ra <134232026+Yuk1ra@users.noreply.github.com>
    Date:   Mon Dec 30 16:12:54 2024 +0200
    
        Added Sidebar
    
    commit 1a653ada2c72f4a96dee39e3fb188e428911959c
    Author: Yuk1ra <134232026+Yuk1ra@users.noreply.github.com>
    Date:   Mon Dec 30 15:28:08 2024 +0200
    
        Logo_changed

--- spotify/src/components/MainPage.jsx
+++ spotify/src/components/MainPage.jsx
@@ -3,12 +3,14 @@ import Header from "./Header"
 import Footer from "./Footer"
 import Main from "./Main"
 import '../style/MainPage.css';
+import Sidebar from "./Sidebar";
 
 export default function MainPage(){
     return(
     <body>
       <Header/> 
       <Main/>
+      <Sidebar/>
       <Aside/>
       <Footer/>
     </body>  
