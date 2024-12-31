6fcd977cd39f72631691c759fc527433536d1c61
hqshe <114933368+hqshe@users.noreply.github.com>
2024-12-31 20:19:35 +0200

    neon lights added

--- spotify/src/components/Footer.jsx
+++ spotify/src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import "../style/Footer.css"
 export default function Footer(){
     return(
         <footer>
-            <p>&copy; 2024 MusicTime. All rights reserved.</p>
+            <p>&copy; 2024 MusicHouse. All rights reserved.</p>
+            <div class="socialmedia">
+                <span class="material-symbols-outlined link" id='youtube'>▷</span>
+                <p class="link" id="facebook">ⓕ</p>
+                <p class="link" id="email">✉</p>
+            </div>
         </footer>
     )
 }
--- spotify/src/components/MainPage.jsx
+++ spotify/src/components/MainPage.jsx
@@ -2,15 +2,18 @@ import Header from "./Header"
 import Footer from "./Footer"
 import Main from "./Main"
 import '../style/MainPage.css';
+import '../style/Footer.css';
 import Sidebar from "./Sidebar";
 
 export default function MainPage(){
     return(
     <body>
-      <Header/> 
-      <Main/>
-      <Sidebar/>
-      <Footer/>
+      <div class="container">
+        <Header/> 
+        <Main/>
+        <Sidebar/>
+        <Footer/>
+      </div>
     </body>  
     )
 }
