ebfd1e4334f3ae3840c642b8fce75901a80abb18
Yuk1ra <134232026+Yuk1ra@users.noreply.github.com>
2024-12-31 19:12:15 +0200

    Commit_Main_changed

--- spotify/src/components/Header.jsx
+++ spotify/src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../style/Header.css';
 export default function Header(){
     return(
         <header class = "header">
-            <h1 data-text = "MusicTime">MusicTime </h1>
+            <h1 data-text = "Music⾕">Music⾕ </h1>
             <NavButtons/>
         </header>
     )
