cfc6278831da46078542b3a13126a8699b4dd3b6
hqshe <114933368+hqshe@users.noreply.github.com>
2025-01-01 16:53:40 +0200

    sidebar realised

--- spotify/src/components/Sidebar.jsx
+++ spotify/src/components/Sidebar.jsx
@@ -4,18 +4,25 @@ import React, { useEffect } from 'react';
 export default function Sidebar() {
     useEffect(() => {
         var button = document.getElementById('sidebarbutton');
-        var playlists = document.getElementsByClassName('playlists');
+        var playlists = document.getElementById('playlists');
+        var sidebar = document.getElementById('sidebar');
         var arrow = document.getElementById('sidebarbuttonarrow');
         button.addEventListener("mouseover", () => {
                 sidebarOpen();
                 rotateArrow(arrow,"180deg");
         });
-        button.addEventListener("mouseleave",() =>{
+        sidebar.addEventListener("mouseleave",() =>{
+            sidebarClose();
             rotateArrow(arrow,"0deg");
         });
 
         function sidebarOpen() {
-            
+            sidebar.style.left = "0";
+            sidebar.style.transition = "all 1s ease";
+        }
+        function sidebarClose(){
+            sidebar.style.left = "-110px";
+            sidebar.style.transition = "all 1s ease";
         }
         function rotateArrow(arrow, degree) {
             if (arrow) {
@@ -26,11 +33,11 @@ export default function Sidebar() {
     }, []);
 
     return (
-        <div className="sidebar">
+        <div className="sidebar" id="sidebar">
             <button className="sidebarbutton" id="sidebarbutton">
                 <p className="sidebarbuttonarrow" id="sidebarbuttonarrow">➣</p>
             </button>
-            <div className="playlists">
+            <div className="playlists" id="playlists">
                 <button className="listbutton">Playlist1</button>
                 <button className="listbutton">Playlist2</button>
                 <button className="listbutton">Playlist3</button>
