677206c5f0874b7f25107cd0679f7b328625bef4
hqshe <114933368+hqshe@users.noreply.github.com>
2025-01-03 21:52:49 +0200

    navbuttons reforked, animation added

--- spotify/src/components/NavButtons.jsx
+++ spotify/src/components/NavButtons.jsx
@@ -1,6 +1,11 @@
+import '../style/NavButtons.css'
 export default function NavButtons(){
     return(
         <nav>
+            <button className="buttonsMain">Music</button>
+        <button className="buttonsMain">Podcasts</button>
+        <button className="buttonsMain">Artists</button>
+        <button className="buttonsMain">Albums</button>
         </nav>
     )
 }
