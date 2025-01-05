fa9f183843a49a2032692892df47dd0fc52aad0c
StasGolouh <134232026+StasGolouh@users.noreply.github.com>
2025-01-06 01:10:16 +0200

    Added_profile_playlists

--- spotify/src/components/Profile.jsx
+++ spotify/src/components/Profile.jsx
@@ -1,16 +1,49 @@
 import '../style/Profile.css';
+import React from 'react';
 
 export default function Profile() {
     return (
         <>
-         <header>
-            <h2 class ="headertext">Welcome, IhorPista</h2>
-            <img src="/images/backgrounds/back-headphones.png" class="backhead"/>
-            <img src="/images/backgrounds/back-nota.png" class="backnota"/>
-        </header>
-        <main>
-            <h1 class ="maintext">YOUR PLAYLISTS</h1>    
-        </main>
+            <header>
+                <h2 className="headertext">Welcome, IhorPista</h2>
+                <img src="/images/backgrounds/back-headphones.png" className="backhead" alt="headphones" />
+                <img src="/images/backgrounds/back-nota.png" className="backnota" alt="nota" />
+            </header>
+
+            <main>
+                <h1 className="maintext">YOUR PLAYLISTS</h1>    
+                <div className="proflists">
+                    <button className="profaudio">
+                        <img src="/images/img1.png" alt="list1" />
+                        <h2>Top 50 - India</h2>
+                    </button>
+
+                    <button className="profaudio">
+                        <img src="/images/img2.png" alt="list2" />
+                        <h2>Nija pearls</h2>
+                    </button>
+
+                    <button className="profaudio">
+                        <img src="/images/img3.png" alt="list3" />
+                        <h2>Today Top Hits</h2>
+                    </button>
+
+                    <button className="profaudio">
+                        <img src="/images/img4.png" alt="list4" />
+                        <h2>Today Top Hits</h2>
+                    </button>
+
+                    <button className="profaudio">
+                        <img src="/images/img5.png" alt="list5" />
+                        <h2>Today Top Hits</h2>
+                    </button>
+
+                    <button className="profaudio">
+                        <img src="/images/img6.png" alt="list6" />
+                        <h2>Today Top Hits</h2>
+                    </button>
+                </div>
+            </main>
         </>
     );
 }
